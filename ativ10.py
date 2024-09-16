89746ae91a204fbc6a5aed5f902c41daa4ac34e9
Evellyn20242008 <Evellynmartins579@gmail.com>
2024-09-16 09:39:39 -0300

    atividade10

---
+++ ativ10.py
@@ -0,0 +1,7 @@
+# Crie um programa que receba um número inteiro e informe se ele é par ou ímpar.
+num1 = int(input("digite o numero para verificação"))
+
+if num1 % 2 == 0:
+    print("esse numero é par")
+else: 
+    print ("esse numero é  impar")
